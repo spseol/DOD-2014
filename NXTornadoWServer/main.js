6dec7abcd98aceb6abd9c91d9a26422cc1b743c4
Josef Kolář <thejoeejoee@gmail.com>
2014-11-25 22:51:39 +0100

    server: refactoring & added logger output to browser

---
+++ NXTornadoWServer/main.js
@@ -0,0 +1,57 @@
+function getLogger() {
+    var ws = new WebSocket('ws://' + window.location.host + '/ws/logger');
+    var $logger = $('#logger');
+    var log = function(content, level, d) {
+        var d = new Date(d*1000);
+        var $time = $('<span></span>').text(d.toLocaleString()).addClass('pull-right');
+        $('<div></div>').prependTo($logger).hide().addClass('text-' + level).text(content).append($time).slideDown(1000);
+    };
+    ws.onmessage = function (evt) {
+        var msg = $.parseJSON(evt.data);
+        console.log(msg);
+        log(msg.content, msg.level, msg.created)
+    };
+    return log;
+};
+
+
+$(function() {
+    var $brickTitleOK = $('#brick-ok');
+    var $brickTitleKO = $('#brick-ko');
+    var $cover = $('#cover');
+    var $form = $('#action-form');
+
+    var ws = new WebSocket('ws://' + window.location.host + '/ws/control');
+    var $number = $('input[type=number]');
+    var $button = $('submit#turn');
+    var log = getLogger();
+    var setBrick = function(enable) {
+        if (enable) {
+            $brickTitleKO.fadeOut(500);
+            $brickTitleOK.fadeIn(500);
+            $cover.fadeOut(200);
+        } else {
+            $brickTitleKO.show();
+            $brickTitleOK.hide();
+            $cover.fadeIn(200);
+        }
+    };
+    setBrick(brickOK);
+    ws.onmessage = function (evt) {
+        var state = $.parseJSON(evt.data);
+        console.log(state);
+        setBrick(state.brick_found);
+    };
+
+    $form.submit(function(event) {
+        event.preventDefault();
+        ws.send($number.val());
+    });
+    exit = function (evt) {
+        setBrick(false);
+        log('Communication error occured!', 'danger');
+    };
+    ws.onclose = exit;
+    ws.onerror = exit;
+
+});
--- NXTornadoWServer/server.py
+++ NXTornadoWServer/server.py
@@ -1,73 +1,114 @@
 import logging
 from logging import Handler, LogRecord, StreamHandler, Formatter, FileHandler
+from tornado import web, ioloop, options, autoreload
 
-from tornado import websocket, web, ioloop, options
 from nxt.locator import find_one_brick, BrickNotFoundError
 from nxt.motor import Motor, PORT_B
+from tornado.web import StaticFileHandler
+from tornado.websocket import WebSocketHandler
+
+
+def init_logging():
+    f = Formatter(fmt='%(asctime)s - %(levelname)s - %(message)s', datefmt='%d. %m. %Y - %H:%M:%S')
+    fh = FileHandler('log.log')
+    wsh = WebSocketsLogHandler()
+    sh = StreamHandler()
+    root_logger = logging.getLogger('')
+    for handler in (fh, wsh, sh):
+        handler.setFormatter(f)
+        root_logger.addHandler(handler)
+
+
+def on_reload():
+    if BrickController.brick_found:
+        BrickController.brick_found = BrickController.brick_searching = False
+        BrickController.brick.sock.close()
+        logging.info('reloading')
+
 
 class BrickController():
     brick = None
     brick_found = False
     motor = None
-    searching = False
+    brick_searching = False
 
     @classmethod
     def init_brick(cls):
-        if cls.searching or cls.brick_found:
+        if cls.brick_found:
+            return cls.brick_found
+        if cls.brick_searching:
             logging.warning('Aborting request for brick searching.')
-            return
-        logging.info('Starting brick searching.')
-        cls.searching = True
+            return cls.brick_found
+        logging.info('Starting new brick brick_searching.')
+        cls.brick_searching = True
         try:
-            raise BrickNotFoundError
+            # raise BrickNotFoundError
             cls.brick = find_one_brick()
         except BrickNotFoundError:
             logging.warning('Brick not found.')
-            return
-        cls.searching, cls.brick_found = True, False
+            cls.brick_searching, cls.brick_found = False, False
+            return cls.brick_found
+        cls.brick_searching, cls.brick_found = False, True
         logging.info('Brick successfully found.')
-
+        cls.init_motors()
+        return cls.brick_found
 
     @classmethod
     def init_motors(cls):
         cls.motor = Motor(cls.brick, PORT_B)
         assert isinstance(cls.motor, Motor)
 
+    @classmethod
+    def get_state(cls):
+        return {
+            'brick_found': cls.brick_found,
+            'brick_searching': cls.brick_searching
+        }
+
 
 class WebSocketsLogHandler(Handler):
-    ws_clients = []
     level = logging.INFO
+    dicts = []
 
     def emit(self, record):
         assert isinstance(record, LogRecord)
-        for client in self.ws_clients:
-            client.write_message(record.msg)
+        msg_dict = {
+            'content': record.msg % record.args,
+            'level': record.levelname.lower(),
+            'created': record.created
+
+        }
+        self.dicts.append(msg_dict)
+        for client in LoggerSocketHandler.clients:
+            client.write_message(msg_dict)
 
 
 class IndexHandler(web.RequestHandler):
     def get(self):
         logging.info('Serving index.html.')
         self.render("index.html", brick_found=BrickController.brick_found,
-                    ws_clients=len(WebSocketsLogHandler.ws_clients))
+                    ws_clients=len(ControlSocketHandler.clients))
 
 
-class SocketHandler(websocket.WebSocketHandler):
+class ControlSocketHandler(WebSocketHandler):
+    clients = []
+
     def open(self):
-        if self not in WebSocketsLogHandler.ws_clients:
-            WebSocketsLogHandler.ws_clients.append(self)
-        logging.info('New WS opening, {} sockets active.'.format(len(WebSocketsLogHandler.ws_clients)))
+        if self not in self.clients:
+            self.clients.append(self)
+        logging.info('New WS opening, {} sockets active.'.format(len(self.clients)))
         if not BrickController.brick_found:
-            BrickController.init_brick()
-            if BrickController.brick_found:
-                self.write_message('brick-ok')
+            s = BrickController.init_brick()
+            for cl in self.clients:
+                cl.write_message(BrickController.get_state())
 
     def on_close(self):
-        if self in WebSocketsLogHandler.ws_clients:
-            WebSocketsLogHandler.ws_clients.remove(self)
-        logging.info('WS closed, {} sockets active.'.format(len(WebSocketsLogHandler.ws_clients)))
+        if self in self.clients:
+            self.clients.remove(self)
+        logging.info('WS closed, {} sockets active.'.format(len(self.clients)))
 
     def on_message(self, message):
-        self.write_message('ok!')
+        logging.info('Received message:\n"{}"'.format((message)))
         if BrickController.brick_found:
             try:
                 deg = int(message)
@@ -76,21 +117,36 @@ class SocketHandler(websocket.WebSocketHandler):
             BrickController.motor.turn(100, deg)
 
 
+class LoggerSocketHandler(WebSocketHandler):
+    clients = []
+
+    def open(self):
+        if self not in self.clients:
+            self.clients.append(self)
+        for d in WebSocketsLogHandler.dicts:
+            self.write_message(d)
+
+    def on_close(self):
+        if self in self.clients:
+            self.clients.remove(self)
+
+    def on_message(self, message):
+        pass
+
+
 app = web.Application([
     (r'/', IndexHandler),
-    (r'/ws', SocketHandler),
-])
+    (r'/ws/control', ControlSocketHandler),
+    (r'/ws/logger', LoggerSocketHandler),
+    (r'/(.*)', StaticFileHandler, {"path": "../NXTornadoWServer"})
+                      ], log_function=lambda x: x)
 
 if __name__ == '__main__':
-    f = Formatter(fmt='%(asctime)s - %(levelname)s - %(message)s', datefmt='%d. %m %Y - %H:%M:%S')
-    fh = FileHandler('log.log')
-    wsh = WebSocketsLogHandler()
-    sh = StreamHandler()
-    root_logger = logging.getLogger('')
-    for handler in (fh, wsh, sh):
-        handler.setFormatter(f)
+    init_logging()
     options.parse_command_line()
     app.listen(8888)
     ioloop = ioloop.IOLoop().instance()
-    # autoreload.start(ioloop)
+    autoreload.add_reload_hook(on_reload)
+    autoreload.start(ioloop)
     ioloop.start()
+    logging.info('Server starting.')
