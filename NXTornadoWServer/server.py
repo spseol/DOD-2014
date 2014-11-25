f5a028cfa2a07b2d5098bf26cb4ea6ac7d1c932c
JoeE Kolář <thejoeejoee@gmail.com>
2014-11-25 14:32:56 +0100

    server: added logging

--- NXTornadoWServer/server.py
+++ NXTornadoWServer/server.py
@@ -1,12 +1,9 @@
-from tornado import websocket, web, ioloop, options
-from tornado import autoreload
+import logging
+from logging import Handler, LogRecord, StreamHandler, Formatter, FileHandler
 
+from tornado import websocket, web, ioloop, options
 from nxt.locator import find_one_brick, BrickNotFoundError
 from nxt.motor import Motor, PORT_B
-
-
-ws_clients = []
-
 
 class BrickController():
     brick = None
@@ -14,45 +11,60 @@ class BrickController():
     motor = None
     searching = False
 
-
     @classmethod
     def init_brick(cls):
-        """
-        decompose this!
-        """
-        if cls.searching:
-            print(' *** ABORTING REQUEST FOR BRICK *** ')
+        if cls.searching or cls.brick_found:
+            logging.warning('Aborting request for brick searching.')
             return
+        logging.info('Starting brick searching.')
+        cls.searching = True
         try:
-            print(' *** BRICK FINDING *** ')
-            cls.searching = True
+            raise BrickNotFoundError
             cls.brick = find_one_brick()
         except BrickNotFoundError:
+            logging.warning('Brick not found.')
             return
-        cls.searching = False
-        print(' *** BRICK FOUND *** ')
+        cls.searching, cls.brick_found = True, False
+        logging.info('Brick successfully found.')
+
+
+    @classmethod
+    def init_motors(cls):
         cls.motor = Motor(cls.brick, PORT_B)
         assert isinstance(cls.motor, Motor)
-        cls.brick_found = True
+
+
+class WebSocketsLogHandler(Handler):
+    ws_clients = []
+    level = logging.INFO
+
+    def emit(self, record):
+        assert isinstance(record, LogRecord)
+        for client in self.ws_clients:
+            client.write_message(record.msg)
 
 
 class IndexHandler(web.RequestHandler):
     def get(self):
-        self.render("index.html", brick_found=BrickController.brick_found, ws_clients=len(ws_clients))
+        logging.info('Serving index.html.')
+        self.render("index.html", brick_found=BrickController.brick_found,
+                    ws_clients=len(WebSocketsLogHandler.ws_clients))
 
 
 class SocketHandler(websocket.WebSocketHandler):
     def open(self):
+        if self not in WebSocketsLogHandler.ws_clients:
+            WebSocketsLogHandler.ws_clients.append(self)
+        logging.info('New WS opening, {} sockets active.'.format(len(WebSocketsLogHandler.ws_clients)))
         if not BrickController.brick_found:
             BrickController.init_brick()
             if BrickController.brick_found:
                 self.write_message('brick-ok')
-        if self not in ws_clients:
-            ws_clients.append(self)
 
     def on_close(self):
-        if self in ws_clients:
-            ws_clients.remove(self)
+        if self in WebSocketsLogHandler.ws_clients:
+            WebSocketsLogHandler.ws_clients.remove(self)
+        logging.info('WS closed, {} sockets active.'.format(len(WebSocketsLogHandler.ws_clients)))
 
     def on_message(self, message):
         self.write_message('ok!')
@@ -70,8 +82,15 @@ app = web.Application([
 ])
 
 if __name__ == '__main__':
+    f = Formatter(fmt='%(asctime)s - %(levelname)s - %(message)s', datefmt='%d. %m %Y - %H:%M:%S')
+    fh = FileHandler('log.log')
+    wsh = WebSocketsLogHandler()
+    sh = StreamHandler()
+    root_logger = logging.getLogger('')
+    for handler in (fh, wsh, sh):
+        handler.setFormatter(f)
     options.parse_command_line()
     app.listen(8888)
     ioloop = ioloop.IOLoop().instance()
-    autoreload.start(ioloop)
+    # autoreload.start(ioloop)
     ioloop.start()
