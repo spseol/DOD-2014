cc1ea6c2872aaaf83caaccbe397a725d05fd508c
Josef Kolář <thejoeejoee@gmail.com>
2014-11-26 22:29:13 +0100

    RPI: changes in time measurement

--- RPITimer/rpi_timer.py
+++ RPITimer/rpi_timer.py
@@ -2,32 +2,40 @@ from Crypto.Random.random import randint
 from requests.api import post
 from time import sleep
 
-from datetime import datetime, timedelta
+from datetime import datetime
 
 
 CONTROL_PORT = 6
 GPIO_DELAY = 0.5  # #delay for GPIO
-url = raw_input('IP:port to control server >>> ')
-url = ''.join(('http://', url, '/control/timer'))
+start_time = None
 
 def is_port_active(port):
     # some magic about GPIO
     r = randint(0, 1000)
     return r > 999
+    # some magic about GPIO
 
 
-start_time = None
+def print_elapsed(td):
+    print('Elapsed {:.3}s.'.format(td.seconds + td.microseconds / 10.0 ** 6))
+
+
+url = raw_input('IP:port to control server (empty for localhost:8888) >>> ') or 'localhost:8888'
+url = ''.join(('http://', url, '/control/timer'))
+
 while 1:
+    # sleep(0.005)
+    if is_port_active(CONTROL_PORT):
+        if start_time is None:
+            start_time = datetime.now()
+            sleep(GPIO_DELAY)
+            continue
+        else:
+            lap_time = datetime.now() - start_time
+            start_time = None
+            miliseconds = lap_time.seconds * 1000 + lap_time.microseconds / 1000.0
+            r = post(url, data={'time': miliseconds})
+            raw_input('end! enter to continue..')
 
-    if is_port_active(CONTROL_PORT) and start_time is None:
-        start_time = datetime.now()
-        sleep(GPIO_DELAY)
-        continue
-
-    if is_port_active(CONTROL_PORT) and start_time is not None:
-        lap_time = datetime.now() - start_time
-        start_time = None
-        milisecs = lap_time.seconds*1000 + lap_time.microseconds/1000.0
-        r = post(url, data={'time': milisecs})
-
+        # TODO: add printing
 
