b16830fbdfc5922489a9bb099dfe12fcedcfaac4
Joe Kolář <thejoeejoee@gmail.com>
2014-11-30 15:54:04 +0100

    server: refactored processing commands in brick controller

--- NXTornadoWServer/NXTornadoWServer/BrickController.py
+++ NXTornadoWServer/NXTornadoWServer/BrickController.py
@@ -7,12 +7,16 @@ from nxt.motor import Motor, PORT_A, PORT_B, BlockedException
 class BrickController():
     brick = None
     brick_found = False
-    motor = None
     brick_searching = False
     steering_motor = None
-    steering_degs = 0
+    actual_steering_degs = 0
     STEERING_KEY = 'steering'
     main_motor = None
+    last_commands = {
+        'steering': 0,
+        'trottle': 0,
+        'reverse': 0,
+    }
     TROTTLE_KEY = 'trottle'
     REVERSE_KEY = 'reverse'
     FULL_SIDE_STEER = 1400
@@ -46,49 +50,61 @@ class BrickController():
 
     @classmethod
     def process(cls, **commands):
-	if not cls.brick_found:
-	    return
-        for key, value in commands.items():
-            if getattr(cls, key, value) != value:
-                #proccess new value
-                print('{} new = {}'.format(key, value))
-                if key == cls.STEERING_KEY:
-                    assert isinstance(cls.steering_motor, Motor)
-                    want_degs = int(value * cls.FULL_SIDE_STEER)
-                    if cls.steering_degs == want_degs:
-                        continue
-                    elif want_degs > cls.steering_degs:
-                        tacho = 100
-                        degs = want_degs - cls.steering_degs
-                    elif want_degs < cls.steering_degs:
-                        tacho = -100
-                        degs = abs(want_degs - cls.steering_degs)
-                    logging.info('{} to {}'.format(degs, ('left', 'right')[tacho > 0]))
-                    try:
-                        cls.steering_motor.turn(tacho, degs, False)
-                        cls.steering_degs = want_degs
-                    except BlockedException:
-                        logging.warning('Steering motor blocked!')
-                    print(cls.steering_motor._get_state().to_list())
-                if key in (cls.REVERSE_KEY, cls.TROTTLE_KEY):
-                    trottle = int(commands[cls.TROTTLE_KEY]*127/100.0)
-                    if trottle == 0:
-                        cls.main_motor.brake()
-                    elif commands[cls.REVERSE_KEY] == 0:
-                       	cls.main_motor.run(trottle)
-                    elif commands[cls.REVERSE_KEY] == 1:
-                       	cls.main_motor.run(-trottle)
-                    else:
-                        logging.danger('Unknown trottle value.')
-            else:
-                #same as last
-                print('{} same'.format(key, value))
-            setattr(cls, key, value)
+        if not cls.brick_found:
+            return
+        if commands[cls.STEERING_KEY] != cls.last_commands[cls.STEERING_KEY]:
+            abs_degs = int(commands[cls.STEERING_KEY] * cls.FULL_SIDE_STEER)
+            cls.set_steering(abs_degs)
+            logging.info('New steering!')
+        if commands[cls.TROTTLE_KEY] != cls.last_commands[cls.TROTTLE_KEY] or commands[cls.REVERSE_KEY] != cls.last_commands[cls.REVERSE_KEY]:
+            cls.set_trottle(commands[cls.TROTTLE_KEY], commands[cls.REVERSE_KEY])
+            logging.info('New trottle or reverse!')
+        cls.last_commands = commands
+
+    @classmethod    
+    def set_steering(cls, abs_degs):
+        assert isinstance(cls.steering_motor, Motor)
+        if cls.actual_steering_degs == abs_degs:
+            logging.warn('Fucks in steering alg.')
+            return
+        elif abs_degs > cls.actual_steering_degs:
+            tacho = 100
+            degs = abs_degs - cls.actual_steering_degs
+        elif abs_degs < cls.actual_steering_degs:
+            tacho = -100
+            degs = abs(abs_degs - cls.actual_steering_degs)
+        logging.info('Steer {} degs to {}.'.format(degs, ('left', 'right')[tacho > 0]))
+        try:
+            cls.steering_motor.weak_turn(tacho, degs)
+            cls.actual_steering_degs = abs_degs
+        except BlockedException:
+            logging.warning('Steering motor blocked!')
+    
+    @classmethod
+    def set_trottle(cls, trottle, reverse):
+        if not reverse in (0, 1):
+            logging.warn('Unknown reverse command, setting to default!')
+            reverse = 0
+        if not(trottle <= 100 and trottle >= 0):
+            logging.warn('Unknown trottle command, setting to 0!')
+            trottle = 0.0
+
+        if trottle <= 0:
+            cls.main_motor.brake()
+            return
+        # <0,100> trans to <(0.7*127),127>
+        motor_trottle = int((0.7 + 0.3 * (trottle / 100.0)) * 127)
+        if not reverse:
+            cls.main_motor.run(motor_trottle)
+        else:
+            cls.main_motor.run(-motor_trottle)
+
 
 
     @classmethod
     def get_state(cls):
         return {
             'brick_found': cls.brick_found,
-            'brick_searching': cls.brick_searching
+            'brick_searching': cls.brick_searching,
+            'steering_motor': cls.steering_motor._get_state().to_list() if cls.steering_motor else []
         }
