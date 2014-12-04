12c7cf9bc27e4c60d7b5eac5e8db5b7b62291f9c
Sony Pony <sirarseus@gmail.com>
2014-12-04 20:09:25 +0100

    Fix C++: references replaced by POINTERS

--- LEDMatrix/main.cpp
+++ LEDMatrix/main.cpp
@@ -8,29 +8,29 @@ class Matrix
 {
 
 	private:
-		volatile uint8_t &m_rowPort;
-		volatile uint8_t &m_colPort;
+		volatile uint8_t *m_rowPort;
+		volatile uint8_t *m_colPort;
 	
 	public:
-		Matrix(volatile uint8_t &rowPort, volatile uint8_t &rowDDR, volatile uint8_t &colPort, volatile uint8_t &colDDR);
-		~Matrix() {}
+		Matrix(volatile uint8_t *rowPort, volatile uint8_t *rowDDR, volatile uint8_t *colPort, volatile uint8_t *colDDR);
+		~Matrix() { m_rowPort = 0; m_colPort = 0; }
 		
 		void setLED(uint8_t x, uint8_t y);
 };
 
-Matrix::Matrix(volatile uint8_t &rowPort, volatile uint8_t &rowDDR, volatile uint8_t &colPort, volatile uint8_t &colDDR)
+Matrix::Matrix(volatile uint8_t *rowPort, volatile uint8_t *rowDDR, volatile uint8_t *colPort, volatile uint8_t *colDDR)
 {
 	m_rowPort = rowPort;
 	m_colPort = colPort;
-	rowDDR = 0xFF;
-	colDDR = 0xFF;
+	*rowDDR = 0xFF;
+	*colDDR = 0xFF;
 }
 
 void Matrix::setLED(uint8_t x, uint8_t y)
 {
-	m_rowPort = 1 << (x - 1);
-	m_colPort = 0xFF;
-	m_colPort &= ~(1 << (y - 1));
+	*m_rowPort = 1 << (x - 1);
+	*m_colPort = 0xFF;
+	*m_colPort &= ~(1 << (y - 1));
 }
 
 //-----------------------------------------
@@ -49,7 +49,7 @@ class Button
 		Button(volatile uint8_t *port, volatile uint8_t *DDR);
 		~Button() {}
 		
-		void control()
+		void control(void )
 }
 //-----------------------------------------
 //-----------------------------------------
